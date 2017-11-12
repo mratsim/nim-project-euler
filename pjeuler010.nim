0d4b5c08cdda6663ed642dca6899818c1f07a145
mratsim <mamy.ratsimbazafy_dev@gadz.org>
2017-11-12 22:35:24 +0100

    Reorg files

--- pjeuler010.nim
+++
@@ -1,8 +0,0 @@
-# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
-
-# Find the sum of all the primes below two million.
-
-from lib_euler_primes import primeSieve
-from math import sum
-
-echo primeSieve(2_000_000).sum
