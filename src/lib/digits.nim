0a892e21b5afb4fc43e766a04a0c36f2d9734e0d
mratsim <mamy.ratsimbazafy_dev@gadz.org>
2017-11-13 21:15:26 +0100

    Add project euler 13

--- src/lib/digits.nim
+++ src/lib/digits.nim
@@ -1,10 +1,15 @@
 from functional import unfold
 from options import Option, some, none
 
-proc divmod10(n: int): Option[(int, int)] =
+proc divmod10_opt(n: int): Option[(int, int)] =
     if n == 0:
         return none((int,int))
-    return some(( (n mod 10).int(), n div 10)) #TODO: understand the weird type of n mod 10
+    return some((n mod 10, n div 10))
 
 proc toDigits*(n: int): seq[int] =
-    unfold(divmod10,n)
+    unfold(divmod10_opt,n)
+
+proc divmod10*(n: int): tuple[rem: int, quot: int] =
+    if n == 0:
+        return (0, 0)
+    return (n mod 10, n div 10)
