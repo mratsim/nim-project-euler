09c6bf59d974b530e0a4a2b64aa1ce263214d0f9
mratsim <mamy.ratsimbazafy_dev@gadz.org>
2017-11-15 01:00:02 +0100

    Rename files with title

--- src/pe008_largest_product_in_a_series.nim
+++ src/pe008_largest_product_in_a_series.nim
@@ -1,3 +1,5 @@
+## Largest product in a series
+
 # The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
 #                73167176531330624919225119674426574742355349194934
 #                96983520312774506326239578318016984801869478851843
--- src/pe013_large_sum.nim
+++ src/pe013_large_sum.nim
@@ -1,3 +1,5 @@
+## Large sum
+#
 # Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
 
 let nbs = [
